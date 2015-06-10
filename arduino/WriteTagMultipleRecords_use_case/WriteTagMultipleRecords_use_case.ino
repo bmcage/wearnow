603e3542d4079a3eabb6bd25c9d2bd15d64ef959
Mahmood Ahmed <Mahmood.Ahmed@ugent.be>
2015-06-10 17:33:13 +0200

    original arduino sketches to read NFC

---
+++ arduino/WriteTagMultipleRecords_use_case/WriteTagMultipleRecords_use_case.ino
@@ -0,0 +1,43 @@
+#if 0
+#include <SPI.h>
+#include <PN532_SPI.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_SPI pn532spi(SPI, 10);
+NfcAdapter nfc = NfcAdapter(pn532spi);
+#else
+
+#include <Wire.h>
+#include <PN532_I2C.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_I2C pn532_i2c(Wire);
+NfcAdapter nfc = NfcAdapter(pn532_i2c);
+#endif
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("NDEF Writer");
+    nfc.begin();
+}
+
+void loop() {
+    Serial.println("\nPlace a formatted Mifare Classic NFC tag on the reader.");
+    if (nfc.tagPresent()) {
+        NdefMessage message = NdefMessage();
+          message.addTextRecord("Clothing ID = SW-05!");
+        message.addTextRecord("Clothing type = Long sleeve shirt!");
+        message.addTextRecord("Thermal insulation = 0.33 clo!");
+       message.addTextRecord("Moisture Vapor resistance = 0.0044 m2kPa/W!");
+    
+        boolean success = nfc.write(message);
+        if (success) {
+            Serial.println("Success. Try reading this tag with your phone.");
+        } else {
+            Serial.println("Write failed");
+        }
+    }
+    delay(3000);
+}
