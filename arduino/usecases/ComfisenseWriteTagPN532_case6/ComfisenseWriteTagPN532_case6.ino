a228c04433551477c373f14f2e6e87af2187a9d0
Mahmood Ahmed <Mahmood.Ahmed@ugent.be>
2015-06-24 11:28:19 +0200

    use cases for testing

--- arduino/usecases/ComfisenseWriteTagPN532_case6/ComfisenseWriteTagPN532_case6.ino
+++ arduino/usecases/ComfisenseWriteTagPN532_case6/ComfisenseWriteTagPN532_case6.ino
@@ -27,14 +27,14 @@ void loop() {
     Serial.println("\nPlace a formatted Mifare Classic NFC tag on the reader.");
     if (nfc.tagPresent()) {
       NdefMessage message = NdefMessage();
-      message.addTextRecord("ID;I001;;Type;Long Sleeve shirt;;Id;0.33;clo");
+      message.addTextRecord("ID;I006;;Type;Pullover;;Id;0.04;m**2 kPa/W");
       //message.addTextRecord("Type;Long Sleeve shirt");   //Clothing type
       //message.addTextRecord("Id;0.33;clo");              //Thermal insulation;
-      message.addTextRecord("Vres;0.0044;m**2 kPa/W;;Th;0.8;mm;;W;100.0;g;;C;#FF0000;hex");   //Moisture Vapor resistance;
+      message.addTextRecord("Vres;0.0072;m**2 kPa/W;;Th;1.1;mm;;W;100.0;g;;C;#474747;hex");   //Moisture Vapor resistance;
       //message.addTextRecord("Th;0.8;;mm");               //Thickness
       //message.addTextRecord("W;100.0;g");                //Weigth
       //message.addTextRecord("C;#FF0000;hex");            //Color
-      message.addTextRecord("URL;cage.ugent.be/~bm/pics/comfisense/tshirt_case0.png");
+      message.addTextRecord("URL;cage.ugent.be/~bm/pics/comfisense/pullover_case6.png");
 
       boolean success = nfc.write(message);
       if (success) {
